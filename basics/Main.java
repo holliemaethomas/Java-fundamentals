40fd32e7076572e78806b95d183023cfc009b048
holliemaethomas <holliemaethomas85@gmail.com>
2020-01-07 11:56:46 -0800

    lab 01

---
+++ basics/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("main method is working!");
+  }
+}
+public static void pluralize (int, String[]) {
+  if (String.int % 2) {
+
+  }
+}
