b5f80b2721e4e1cb15609368d376e5804c30fe4c
holliemaethomas <holliemaethomas85@gmail.com>
2020-01-15 13:10:05 -0800

    added restaurant constructor

---
+++ inheritance/src/main/java/inheritance/Restaurant.java
@@ -0,0 +1,27 @@
+package inheritance;
+import java.util.List;
+import java.util.LinkedList;
+
+    public class Restaurants {
+        List<Review> reviews;
+        String name;
+        double rating;
+        String priceCategory;
+        public Restaurants(String name, double rating, String priceCategory){
+            reviews = new LinkedList<>();
+            this.name = name;
+            this.rating = rating;
+            this.priceCategory = priceCategory;
+        }
+        public String toString() {
+            return "Restaurant name: "+ this.name +" | Rating: "+ this.rating + " | Price Category " + this.priceCategory;
+        }
+        public void addReview(Review r){
+            this.reviews.add(r);
+            r.restaurant = this;
+        }
+    }
+
+
+
+//large amount of inserted code is because I had to kill my previous file with fire and paste my code back in :)
